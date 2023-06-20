a65eec8837c1d62fdfb6897a73940cda310c858b
Stefano Pisano <s.pisano@techgap.it>
2023-06-20 17:02:47 +0200

    feat(Expenses): render ExpenseItem with javascript map

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -3,25 +3,16 @@ import ExpenseItem from "./ExpenseItem";
 
 function Expenses(props) {
 	const expenses = props.expenses;
+	const expenseItems= expenses.map(expense =>
+		<ExpenseItem title={expense.title}
+		             date={expense.date}
+		             amount={expense.amount}
+		/>
+	)
 
 	return (
 		<div className="expenses">
-			<ExpenseItem title={expenses[0].title}
-			             date={expenses[0].date}
-			             amount={expenses[0].amount}
-			/>
-			<ExpenseItem title={expenses[1].title}
-			             date={expenses[1].date}
-			             amount={expenses[1].amount}
-			/>
-			<ExpenseItem title={expenses[2].title}
-			             date={expenses[2].date}
-			             amount={expenses[2].amount}
-			/>
-			<ExpenseItem title={expenses[3].title}
-			             date={expenses[3].date}
-			             amount={expenses[3].amount}
-			/>
+			{expenseItems}
 		</div>
 	)
 }
