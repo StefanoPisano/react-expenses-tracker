ac283ec05a3517ec4af67177cbc5a0d37c076e64
Stefano Pisano <s.pisano@techgap.it>
2023-06-20 16:41:34 +0200

    feat(ExpenseDate): split ExpenseItem into multiple component, ExpenseDate

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,11 @@
 import './ExpenseItem.css'
+import './ExpenseDate';
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem({date, title, amount}) {
-	const year = date.getFullYear();
-	const month = date.toLocaleString('en-US', {month: 'long'});
-	const day = date.toLocaleString('en-US', {day: '2-digit'});
 	return (
 		<div className="expense-item">
-			<div>
-				<div>{month}</div>
-				<div>{year}</div>
-				<div>{day}</div>
-			</div>
+			<ExpenseDate date={date}/>
 			<div className="expense-item__description">
 				<h2>{title}</h2>
 				<div className="expense-item__price">€ {amount}</div>
