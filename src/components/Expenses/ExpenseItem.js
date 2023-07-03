656f54c386eaf1280b69c1ec964558b43199771d
Stefano Pisano <s.pisano@techgap.it>
2023-07-03 19:26:30 +0200

    fix(ExpenseFilter): remove change title button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,22 +2,14 @@ import './ExpenseItem.css'
 import './ExpenseDate';
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
-import {useState} from "react";
 
 function ExpenseItem({date, title, amount}) {
-	const [titleToDisplay, setTitle] = useState(title);
-
-	function clickHandler() {
-		setTitle("updated!")
-	}
-
 	return (
 		<Card className="expense-item">
 			<ExpenseDate date={date}/>
 			<div className="expense-item__description">
-				<h2>{titleToDisplay}</h2>
+				<h2>{title}</h2>
 				<div className="expense-item__price">€ {amount}</div>
-				<button onClick={clickHandler}>Change Title</button>
 			</div>
 		</Card>
 	)
