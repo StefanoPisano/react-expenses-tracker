09877d0f603da02f32d382077c03b11188179ad1
Stefano Pisano <s.pisano91@gmail.com>
2023-07-08 20:57:33 +0200

    feat(Expenses): remove default expense array, we can start from an empty array

--- src/App.js
+++ src/App.js
@@ -2,30 +2,8 @@ import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 import {useState} from "react";
 
-const DEFAULT_EXPENSES = [
-	{
-		id: crypto.randomUUID(),
-		title: 'Toilet Paper',
-		amount: 94.12,
-		date: new Date(2020, 7, 14),
-	},
-	{id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12)},
-	{
-		id: crypto.randomUUID(),
-		title: 'Car Insurance',
-		amount: 294.67,
-		date: new Date(2021, 2, 28),
-	},
-	{
-		id: crypto.randomUUID(),
-		title: 'New Desk (Wooden)',
-		amount: 450,
-		date: new Date(2021, 5, 12),
-	},
-];
-
 function App() {
-	const [expenses, setExpenses] = useState(DEFAULT_EXPENSES);
+	const [expenses, setExpenses] = useState([]);
 
 	const newExpenseHandler = (expense) => {
 		console.trace("App.js", expense);
